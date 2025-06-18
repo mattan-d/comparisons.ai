5e5c4fd2ff57ebe1b03107233932affe1b6d07ff
Mattan Dor <mattan.dor1@gmail.com>
2025-06-18 23:12:51 +0300

    some

--- classes/init.php
+++ classes/init.php
@@ -5,5 +5,5 @@ include __DIR__ . '/MySQLDB.php';
 $CONFIG = new stdClass();
 $CONFIG->dbhost = '127.0.0.1';
 $CONFIG->dbname = 'dev_comparison';
-$CONFIG->dbuser = 'ravit';
-$CONFIG->dbpass = 'ravit1234!@';
+$CONFIG->dbuser = 'root';
+$CONFIG->dbpass = 'lm77jbt5';
